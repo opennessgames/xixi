73ea7c5dde4ac1b58fbc2d258c0d08d5b3de9fd8
Yatwohee <jisanduo@outlook.com>
2024-11-13 16:30:29 +0800

    2024-11-13

--- src/CRC32/CRC32.h
+++ src/CRC32/CRC32.h
@@ -2,7 +2,7 @@
  * @Author: xixi_
  * @Date: 2024-10-23 20:13:10
  * @LastEditors: xixi_
- * @LastEditTime: 2024-11-13 10:40:55
+ * @LastEditTime: 2024-11-13 16:28:40
  * @FilePath: /FHMF/src/Modules/xixi/src/CRC32/CRC32.h
  * Copyright (c) 2020-2024 by xixi_ , All Rights Reserved.
  */
@@ -21,6 +21,7 @@ extern "C"
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #define CRC32_POLYNOMIAL 0xEDB88320 // CRC32 polynomial used for calculation
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     /**
      * @brief 生成CRC32查找表
      *
@@ -29,7 +30,9 @@ extern "C"
      * @note 建议使用256长度，例如 ThisCrc32Table[256]
      */
     void XIXI_CRC32GenerateTable(uint32_t *ThisCrc32Table);
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     /**
      * @brief 计算文件的CRC32值，并返回格式化后的字符串
      *
@@ -39,7 +42,7 @@ extern "C"
      * @note 如果无法打开文件或分配内存失败，将返回NULL;禁止使用`printf("%s\n",XIXI_CRC32File(ThisFilePath, ThisCrc32Table));`
      */
     char *XIXI_CRC32File(const char *ThisFilePath, uint32_t *ThisCrc32Table);
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     /**
      * @brief 计算大文件的CRC32值，并返回格式化后的字符串（分块读取）
      *
@@ -50,7 +53,7 @@ extern "C"
      * @note 如果无法打开文件或分配内存失败，将返回NULL;禁止使用`printf("%s\n",XIXI_CRC32BigFile(ThisFilePath, ThisCrc32Table,ThisBufferSize));`
      */
     char *XIXI_CRC32BigFile(const char *ThisFilePath, uint32_t *ThisCrc32Table, int ThisBufferSize);
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     /**
      * @brief 计算字符串的CRC32值，并返回格式化后的字符串
      *
